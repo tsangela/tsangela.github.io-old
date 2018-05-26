652c59cad7456bdd70d3a092bcb86be367ad7520
Angela Tsang <tsangangela_@hotmail.com>
2018-05-26 16:29:48 -0700

    Single page layout

--- myScript.js
+++ myScript.js
@@ -1,12 +1,22 @@
-$("p, a, summary, h4, li").hover(function(){
-    $(this).css("background-color", "#D4FAF2");
-    }, function(){
-    $(this).css("background-color", "transparent");
-});
+//$("p, a, summary, h4, li").hover(function(){
+//    $(this).css("background-color", "#D4FAF2");
+//    }, function(){
+//    $(this).css("background-color", "transparent");
+//});
 
 // Links
-$("a").hover(function(){
+$("a, h3").hover(function(){
     $(this).css("font-style", "italic");
     }, function(){
     $(this).css("font-style", "normal");
 });
+
+// Image
+$("img").hover(function(){
+    $(this).css("opacity", 0.5);
+    }, function(){
+    $(this).css("opacity", 1);
+});
+
+// Headers
+// do the # fading thing upon hover
