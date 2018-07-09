1e4dc20e330e82c83573b0b360fc974bc42f8d87
Angela Tsang <tsangangela_@hotmail.com>
2018-07-09 08:25:11 -0700

    Responsive, renamed classes

--- myScript.js
+++ myScript.js
@@ -1,30 +1,30 @@
-//// Italic
-//$("a, h3, summary").hover(function(){
-//    $(this).css("font-style", "italic");
-//    }, function(){
-//    $(this).css("font-style", "normal");
-//});
-//
-//// Bold
-//$("h4").hover(function(){
-//    $(this).css("font-weight", "bold");
-//    }, function(){
-//    $(this).css("font-weight", "normal");
-//});
-//
-//// Links
-//$("a, summary, button").hover(function(){
-//    $(this).css("cursor", "help");
-//    }, function(){
-//    $(this).css("cursor", "help");
-//});
-//
-//// Image
-//$("img").hover(function(){
-//    $(this).css("opacity", 0.5);
-//    }, function(){
-//    $(this).css("opacity", 1);
-//});
+// Italic
+$("a, h3, summary").hover(function(){
+    $(this).css("font-style", "italic");
+    }, function(){
+    $(this).css("font-style", "normal");
+});
+
+// Bold
+$("h4").hover(function(){
+    $(this).css("font-weight", "bold");
+    }, function(){
+    $(this).css("font-weight", "normal");
+});
+
+// Links
+$("a, summary, button").hover(function(){
+    $(this).css("cursor", "help");
+    }, function(){
+    $(this).css("cursor", "help");
+});
+
+// Image
+$("img").hover(function(){
+    $(this).css("opacity", 0.5);
+    }, function(){
+    $(this).css("opacity", 1);
+});
 
 // Carousel
 $("carousel-control-prev, carousel-control-next").hover(function(){
