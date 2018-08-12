ac80644021a2d5d81f47e9f3f4742f7a63b6c86a
Angela Tsang <tsangangela_@hotmail.com>
2018-08-11 23:46:59 -0700

    Updated "updates" section

--- myScript.js
+++ myScript.js
@@ -1,31 +1,3 @@
-//// Italic
-//$("a, h3, summary").hover(function(){
-//    $(this).css("font-style", "italic");
-//    }, function(){
-//    $(this).css("font-style", "normal");
-//});
-//
-//// Bold
-//$("h4").hover(function(){
-//    $(this).css("font-weight", "bold");
-//    }, function(){
-//    $(this).css("font-weight", "normal");
-//});
-//
-//// Links
-//$("a, summary, button").hover(function(){
-//    $(this).css("cursor", "help");
-//    }, function(){
-//    $(this).css("cursor", "help");
-//});
-//
-//// Image
-//$("img").hover(function(){
-//    $(this).css("opacity", 0.5);
-//    }, function(){
-//    $(this).css("opacity", 1);
-//});
-
 // Carousel
 $("carousel-control-prev, carousel-control-next").hover(function(){
     $(this).css("opacity", 0.2);
