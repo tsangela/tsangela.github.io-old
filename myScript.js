08523648c057c661c17a4e0155a634e3baed62dd
Angela Tsang <tsangangela_@hotmail.com>
2018-08-30 21:45:26 -0700

    hide navbar on click out, fixed sizing, other small style details

--- myScript.js
+++ myScript.js
@@ -1,18 +1,11 @@
 // Carousel
-$("carousel-control-prev, carousel-control-next").hover(function(){
-    $(this).css("opacity", 0.2);
-    }, function(){
-    $(this).css("opacity", 1);
-});
-
 $("#hungrycatpreview").carousel('pause');
 
-$(document).click(function(event) {
+$(document).click(function (event) {
     var clickover = $(event.target);
     var $navbar = $(".navbar-collapse");
-    var _opened = $navbar.hasClass("in");
+    var _opened = $navbar.hasClass("show");
     if (_opened === true && !clickover.hasClass("navbar-toggler")) {
         $navbar.collapse('hide');
     }
 });
-
